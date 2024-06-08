4e703b911a86e735339afafe8fc8953e47b234ec
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-08 14:50:32 +0600

    get access token

--- app/(protected)/plans/page.tsx
+++ app/(protected)/plans/page.tsx
@@ -1,10 +1,24 @@
 import PlanContext from "@/components/plan/plan-content"
 
 const Page = async () => {
+
+    const accessTokenResponse = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/oauth/access_token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'client_id': `${process.env.NEXT_PUBLIC_SHOPIFY_API_KEY}`,
+            'client_secret': `${process.env.NEXT_PUBLIC_SHOPIFY_API_SECRET}`,
+            'grant_type': 'client_credentials'
+        })
+    })
+
+    const { access_token } = await accessTokenResponse.json()
     const store_name = await fetch('https://photo-optima.myshopify.com/admin/api/2024-04/shop.json', {
         method: 'GET',
         headers: {
-            'X-Shopify-Access-Token': `${process.env.SHOPIFY_ADMIN_ACCESS_TOKEN}`
+            'X-Shopify-Access-Token': `${access_token}`
         }
     })
 
