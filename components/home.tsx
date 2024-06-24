9be6e6a85dbd5de86b459583c349775ac3661a87
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-24 16:46:33 +0600

    cache invalidation for batch compress and restore

--- components/home.tsx
+++ components/home.tsx
@@ -47,13 +47,17 @@ const Home = ({ store }: { store: any }) => {
 
         backend.on('image_model', () => {
             console.log('backend event')
-            queryClient.invalidateQueries({ queryKey: ["images","getBatchRestoreImageLength","getBatchCompressImageLength"] })
+            queryClient.invalidateQueries({ queryKey: ["images"] })
+            queryClient.invalidateQueries({ queryKey: ["getBatchRestoreImageLength"] })
+            queryClient.invalidateQueries({ queryKey: ["getBatchCompressImageLength"] })
             // queryClient.invalidateQueries({queryKey:['getBatchCompressImageLength']})
         })
 
         mq.on('image_model', () => {
             console.log('mq event')
-            queryClient.invalidateQueries({ queryKey: ["images","getBatchRestoreImageLength","getBatchCompressImageLength"] })
+            queryClient.invalidateQueries({ queryKey: ["images"] })
+            queryClient.invalidateQueries({ queryKey: ["getBatchRestoreImageLength"] })
+            queryClient.invalidateQueries({ queryKey: ["getBatchCompressImageLength"] })
         })
 
         return () => {
