e4156641b680850d91e8ec1a777e6f901289d398
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-26 22:25:56 +0600

    Trying to fix installation error

--- app/(protected)/plans/page.tsx
+++ app/(protected)/plans/page.tsx
@@ -1,52 +1,29 @@
 import PlanContext from "@/components/plan/plan-content"
+import { graphql } from "@/lib/gql/gql";
+import { useLazyQuery } from "@apollo/client";
+const GET_SHOP:any = graphql(`
+    #graphql
+    query getShop {
+      shop {
+        name
+      }
+    }
+  `);
 
 const Page = async () => {
     try {
-        const accessTokenResponse = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/oauth/access_token`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "client_id": process.env.SHOPIFY_CLIENT_ID,
-                "client_secret": process.env.SHOPIFY_CLIENT_SECRET,
-                "grant_type": "client_credentials"
-            })
-        })
 
-        if (!accessTokenResponse.ok) {
-            const errorDetails = await accessTokenResponse.text();
-            console.error('Access token fetch error details:', errorDetails);
-            throw new Error('Failed to fetch access tokensssss');
-        }
+        const [getShop] = useLazyQuery(GET_SHOP, {
+            fetchPolicy: "network-only",
+        });
 
-        const accessToken = await accessTokenResponse.json()
+        const { data, error } = await getShop();
 
-        console.log("invalid token : ", accessToken)
-
-        const storeResponse = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/api/2024-04/shop.json`, {
-            method: 'GET',
-            headers: {
-                'X-Shopify-Access-Token': `${accessToken.access_token}`
-            }
-        })
-
-        if (!storeResponse.ok) {
-            const errorDetails = await storeResponse.text();
-            console.error('Error details:', errorDetails);
-            throw new Error('Failed to fetch shop data')
-        }
-
-        const { shop } = await storeResponse.json()
-
-        if (!shop || !shop.domain) {
-            return (
-                <p>Domain name is not available</p>
-            )
-        }
+        console.log("plan",data)
 
         return (
-            <PlanContext shop={shop.domain} />
+            // <PlanContext shop={shop.domain} />
+            <div></div>
         )
     } catch (error: any) { // Explicitly typing error as 'any' to prevent type issues
         console.error(error)
