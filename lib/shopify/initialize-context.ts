1888c29eed701957f7db803c5683757d120c0b7b
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-26 18:29:41 +0600

    Oauth error invalid_request: The redirect_uri is not whitelisted

--- lib/shopify/initialize-context.ts
+++ lib/shopify/initialize-context.ts
@@ -9,7 +9,7 @@ const shopify = shopifyApi({
   apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY || "",
   apiSecretKey: process.env.NEXT_PUBLIC_SHOPIFY_API_SECRET || "",
   scopes: process.env.SCOPES?.split(",") || ["write_products"],
-  hostName: process.env.HOST?.replace(/http?:\/\//, "") || "",
+  hostName: process.env.HOST?.replace(/https?:\/\//, "") || "",
   hostScheme: "https",
   isEmbeddedApp: true,
   apiVersion: LATEST_API_VERSION,
