ae7c262716a9d7bb22a494c57c41d43f941f5c6a
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-27 12:10:23 +0600

    refactor getShop in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { performChecks } from "@/lib/shopify/shopify-oauth";
 import Home from "@/components/home";
 import { ExitClient } from "@/components/exit-client";
+import { getShop } from "@/actions/get-shop";
+
 
 
 export default async function Page({
@@ -13,28 +15,30 @@ export default async function Page({
   // we can perform some checks to see if the app has been installed and that it is still valid
   const { shop, host, hmac, embedded } = searchParams;
 
-  const accessTokenResponse = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/oauth/access_token`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      "client_id": "0b77a1fb0b2de6c8915e1d2155b34163",
-      "client_secret": "1b6edba642c50854d3bd8f6ed0697c1c",
-      "grant_type": "client_credentials"
-    })
-  })
+  const response = await getShop();
 
-  const { access_token } = await accessTokenResponse.json()
+  // const accessTokenResponse = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/oauth/access_token`, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     "client_id": "0b77a1fb0b2de6c8915e1d2155b34163",
+  //     "client_secret": "1b6edba642c50854d3bd8f6ed0697c1c",
+  //     "grant_type": "client_credentials"
+  //   })
+  // })
 
-  const store_name = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/api/2024-04/shop.json`, {
-    method: 'GET',
-    headers: {
-      'X-Shopify-Access-Token': `${access_token}`
-    }
-  })
+  // const { access_token } = await accessTokenResponse.json()
 
-  const { shop: shop1 } = await store_name.json()
+  // const store_name = await fetch(`https://${process.env.SHOPIFY_STORE_DOMAIN}/admin/api/2024-04/shop.json`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-Shopify-Access-Token': `${access_token}`
+  //   }
+  // })
+
+  // const { shop: shop1 } = await store_name.json()
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_MQSERVER}/store`, {
     method: 'POST',
@@ -42,7 +46,7 @@ export default async function Page({
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      storeName: `${shop1.domain}`
+      storeName: `${response.success}`
     }),
     cache: 'no-store'
   })
