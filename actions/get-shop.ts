853739e30f855dac49bf5331eb7240a5023c82f7
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-30 12:21:15 +0600

    debugging get shopify shop through cookie

--- actions/get-shop.ts
+++ actions/get-shop.ts
@@ -7,7 +7,8 @@ export const getShop = async () => {
     // const { shop:client_shop } = useSessionCheck();
     // console.log("client shop : ",client_shop)
     const cookieStore = cookies();
-    console.log("get shop in server action : ",cookieStore.get("shop"))
+    const shopify_shop_name = cookieStore.get("shop")?.value
+    console.log("shopify_shop_name : ",shopify_shop_name)
     const shopify_shop = process.env.SHOPIFY_STORE_DOMAIN;
     const client_id = process.env.SHOPIFY_CLIENT_ID;
     const client_secret = process.env.SHOPIFY_CLIENT_SECRET;
