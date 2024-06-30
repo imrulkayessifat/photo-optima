ed795f765a3756d7cc5a5dd00a69ba6760ea9afd
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-30 22:27:51 +0600

    trying to get access token

--- providers/apollo-provider.tsx
+++ providers/apollo-provider.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import { useAppBridge } from "@shopify/app-bridge-react";
 import {
   HttpLink,
@@ -6,14 +8,22 @@ import {
   ApolloProvider as ApolloProviderClient,
 } from "@apollo/client";
 
+const config = {
+  // The client ID provided for your application in the Partner Dashboard.
+  apiKey: `${process.env.SHOPIFY_CLIENT_ID}`,
+  // The host of the specific shop that's embedding your app. This value is provided by Shopify as a URL query parameter that's appended to your application URL when your app is loaded inside the Shopify admin.
+  host: new URLSearchParams(location.search).get("host"),
+  forceRedirect: true
+};
+
 export default function ApolloProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const app = useAppBridge();
-  const token = app.idToken();
-  console.log("Token: ", token);
+  // const app = useAppBridge();
+  // const token = app.idToken();
+  // console.log("Token: ", token);
   const http = new HttpLink({
     uri: `/api/graphql`,
     fetch: fetch,
