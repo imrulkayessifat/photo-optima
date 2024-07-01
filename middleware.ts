4d42d032dd43188097ce9f2afed311979758f586
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-01 13:05:53 +0600

    get shop from cookie

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-import { cookies } from "next/headers";
-
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const cookieStore = cookies()
 
-    const allCookies = request.cookies.getAll()
-    console.log("cookies : ",allCookies)
+    const shop = request.cookies.get('shop')
+
+    const authUrl = `${process.env.NEXT_PUBLIC_HOST}/api/auth/callback`
+    if(!shop){
+        window.open(authUrl, "_top");
+    }
     const response = NextResponse.next()
     return response
 }
