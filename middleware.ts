2433ef8ea035b5caa4c614403f21f4696615c1fc
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-02 14:16:35 +0600

    get shop from token and set token to cookie

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { jwtDecode } from "jwt-decode"
+import { cookies } from 'next/headers'
+
+interface TokenProps {
+    iss: string,
+    dest: string,
+    aud: string,
+    sub: string,
+    exp: number,
+    nbf: number,
+    iat: number,
+    jti: string,
+    sid: string,
+    sig: string
+}
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -9,6 +23,8 @@ export function middleware(request: NextRequest) {
     console.log("token", token)
     if (token) {
         console.log("decode token : ", jwtDecode(token as string));
+        const shop: TokenProps = jwtDecode(token as string)
+        cookies().set("shop", shop.dest)
     }
 
 }
