23b7440fe2686c738febac3383b2b7a29b9cfbef
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-02 15:16:58 +0600

    get shop from token and set token to cookie

--- actions/set-shop.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-import { cookies } from 'next/headers'
-
-export const setShop = async (shop: string) => {
-    cookies().set("shop", shop)
-}
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { ExitClient } from "@/components/exit-client";
 import { getShop } from "@/actions/get-shop";
 import { cookies } from "next/headers";
 import ReloadWindow from "@/components/reload-window";
-import { setShop } from "@/actions/set-shop";
 import { headers } from "next/headers";
 
 export default async function Page({
@@ -17,9 +16,6 @@ export default async function Page({
   // we can perform some checks to see if the app has been installed and that it is still valid
   const { shop, host, hmac, embedded } = searchParams;
 
-  // if (shop) {
-  //   await setShop(shop as string);
-  // }
 
   // if (!cookieStore.get("shop")) {
   //   return (
