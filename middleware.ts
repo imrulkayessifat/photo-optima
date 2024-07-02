cc0fde40a007ea8d7e41dde34777cfb1496807f6
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-02 13:15:23 +0600

    Cookies can only be modified in a Server Action or Route Handler.

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,6 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-
-    console.log("request : ",request.cookies.getAll())
+    const requestHeaders = new Headers(request.headers)
+    console.log("request : ",requestHeaders)
 }
