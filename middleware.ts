0f2d0679fdd154bfb2b48f11e01c0e054051668a
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-02 14:08:20 +0600

    [InvalidTokenError]: Invalid token specified: must be a string

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import {jwtDecode} from "jwt-decode"
+import { jwtDecode } from "jwt-decode"
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const requestHeaders = new Headers(request.headers)
     const token = requestHeaders.get("authorization")
-    if(!token){
-        console.log("decode token : ",jwtDecode(token as string));
+    console.log("token", token)
+    if (!token) {
+        console.log("decode token : ", jwtDecode(token as string));
     }
 
 }
