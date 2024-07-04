201626cd7642f83368274d03a9bc4144fc5b898e
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-05 00:17:33 +0600

    fix path name

--- components/push-main.tsx
+++ components/push-main.tsx
@@ -6,6 +6,7 @@ import { useRouter,usePathname } from 'next/navigation'
 const PushMain = () => {
     const router = useRouter();
     const pathName = usePathname();
+    console.log("path name : ",pathName)
 
     useEffect(() => {
         router.push(pathName)
