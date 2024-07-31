ad4eaae604b9856928c9cb26e049bafcbc88f85d
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-31 20:54:13 +0600

    Error: shopify.config can't be used in a server environment. You likely need to move this code into an Effect.

--- hooks/use-app-router.ts
+++ hooks/use-app-router.ts
@@ -2,17 +2,19 @@
 
 import { useAppBridge } from "@shopify/app-bridge-react";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export function useAppRouter() {
     const router = useRouter();
     const shopify = useAppBridge();
-    //   const push = (url: string) => {
-    //     const query = new URLSearchParams();
-    //     query.append("shop", shopify.config.shop);
-    //     query.append("host", shopify.config.host || "");
-    //     router.push(`${url}?${query.toString()}`);
-    //   };
-    //   return { push, router };
-    const shop = shopify.config.shop
-    return { shop }
+    const [shop, setShop] = useState("");
+
+    useEffect(() => {
+        if (shopify && shopify.config) {
+            setShop(shopify.config.shop);
+        }
+    }, [shopify]);
+
+    return { shop, router };
 }
+
