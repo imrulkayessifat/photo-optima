3d46fa7a5885ff2177e69f16c17feaf52e7407dd
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-31 18:24:42 +0600

    debug cookie ....

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Page({
 }) {
   // we can perform some checks to see if the app has been installed and that it is still valid
   const { shop, host, hmac, embedded } = searchParams;
-  console.log("shop", typeof shop, cookies().get("shop")!.value)
+  console.log("shop", typeof shop,shop, cookies().get("shop")!.value)
   console.log(cookies().get("shop")!.value !== shop)
 
   if (cookies().get("shop")!.value !== shop) {
