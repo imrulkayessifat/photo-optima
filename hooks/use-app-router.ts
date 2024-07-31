8c3ac5ef6aff24b780d7f128631cb8bdd0123c45
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-31 17:11:46 +0600

    debug-disable middleware caching

--- hooks/use-app-router.ts
+++ hooks/use-app-router.ts
@@ -1,6 +1,7 @@
 import { useAppBridge } from "@shopify/app-bridge-react";
 
 export function useAppRouter() {
+    const shopify = useAppBridge();
     const shop = shopify.config.shop;
     return { shop }
 }
