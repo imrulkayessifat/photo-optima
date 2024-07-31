57aabf7760efce89875900564331fe6cabf1ef6c
imrulkayessifat <kayesimrul224@gmail.com>
2024-07-31 18:44:40 +0600

    reload window -- debug

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,12 @@ export default async function Page({
   // we can perform some checks to see if the app has been installed and that it is still valid
   const { shop, host, hmac, embedded } = searchParams;
 
+  if (cookies().get("shop")?.value !== shop) {
+    return (
+      <ReloadWindow />
+    )
+  }
+
   if (!cookies().get("shop")) {
     return (
       <ReloadWindow />
