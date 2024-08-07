9467c8924b3750618070d6b66f49f096c5b5fb99
imrulkayessifat <kayesimrul224@gmail.com>
2024-08-07 16:43:42 +0600

    trying to register webhooks

--- lib/shopify/register-webhooks.ts
+++ lib/shopify/register-webhooks.ts
@@ -13,12 +13,23 @@ export function addHandlers() {
       ["APP_UNINSTALLED"]: {
         deliveryMethod: DeliveryMethod.Http,
         callbackUrl: "/api/webhooks",
-        callback: async (_topic, shop, _body) => {
-          console.log("Uninstalled app from shop: " + shop);
-          await AppInstallations.delete(shop);
-        },
+        // callback: async (_topic, shop, _body) => {
+        //   console.log("Uninstalled app from shop: " + shop);
+        //   await AppInstallations.delete(shop);
+        // },
       },
     });
+    shopify.webhooks.addHandlers({
+      PRODUCTS_CREATE: [{
+        deliveryMethod: DeliveryMethod.Http,
+        callbackUrl: "https://app1.photooptima.com/webhooks/product/create",
+      }],
+      PRODUCTS_UPDATE: [{
+        deliveryMethod: DeliveryMethod.Http,
+        callbackUrl: "https://app1.photooptima.com/webhooks/product/update",
+      }],
+    });
+    
     console.log("Added handlers");
   } else {
     console.log("Handlers already added");
@@ -27,7 +38,7 @@ export function addHandlers() {
 
 export async function registerWebhooks(session: Session) {
 
-  console.log("registerWebhooks :",session)
+  console.log("registerWebhooks :", session)
   addHandlers();
   const responses = await shopify.webhooks.register({ session });
   console.log("Webhooks added", responses);
