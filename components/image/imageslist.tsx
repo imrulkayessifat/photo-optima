f81de459bce98f26316d1f91b100c05a67bb727c
imrulkayessifat <kayesimrul224@gmail.com>
2024-08-18 14:35:58 +0600

    manage product image for your store

--- components/image/imageslist.tsx
+++ components/image/imageslist.tsx
@@ -27,7 +27,7 @@ const ImagestList: React.FC<ProductListDataProps> = ({
             <div className='flex items-center justify-between'>
                 <Heading
                     title={`Images List (${images.length})`}
-                    description={`Manage Images for ${storeName}`}
+                    description={`Manage products images for your store`}
                 />
             </div>
             <Separator />
